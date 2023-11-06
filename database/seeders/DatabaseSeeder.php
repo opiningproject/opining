e3d1161717a99c76d7b1fa12c3ed96bcdc1b781e
Rekha Sharma <rekhas.inheritx@gmail.com>
2023-11-06 15:16:20 +0530

    - User Seed Changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,12 +15,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         DB::table('users')->insert([
-            'first_name' => 'Restuarant Name',
-            'last_name' => '',
+            'name' => 'Restuarant Name',
             'email' => 'admin@admin.com',
             'password' => Hash::make('Inx@!123'),
             'user_role' => 1,
-            'verified_email' => 1,
             'created_at' => date('Y-m-d h:i:s'),
             'updated_at' => date('Y-m-d h:i:s'),
         ]);

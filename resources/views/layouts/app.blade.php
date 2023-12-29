5b442a19ddb0416536d02f314b629e8d0e2ccfae
Rekha Sharma <rekhas.inheritx@gmail.com>
2023-12-29 15:48:42 +0530

    - Admin Loader Design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,9 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 @include('layouts.admin.header')
 <body>
-
+<div class="position-fixed inset-0 bg-transparent-layer" style="display:none;" id="loader">
+    <img src="{{ asset('images/loader.gif') }}" style="height: 250px">
+</div>
 @yield('content')
 @include('layouts.admin.footer')
 @yield('script')

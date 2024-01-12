3a3a288d40f96bb5de75ec1a2a2a83ff5aefd901
Rekha Sharma <rekhas.inheritx@gmail.com>
2024-01-12 15:34:45 +0530

    - Footer Logo Design change

--- resources/views/layouts/admin/footer_design.blade.php
+++ resources/views/layouts/admin/footer_design.blade.php
@@ -2,7 +2,7 @@
     <div class="footer-logo d-flex justify-content-center align-items-center">
         <a href="{{ route('home') }}" style="max-width: 175px;">
             <p class="mb-0">Gomeal<span class="text-yellow-1">.</span></p>
-            <div class="text-start" style="padding-left: 60px; padding-right: 15px; margin-top: -15px;">
+            <div class="text-start d-flex" style="padding-left: 60px; padding-right: 15px; margin-top: -15px;">
                 <img src="{{ Auth::user()->restaurantDetails->restaurant_logo }}" style="max-width: 100%;">
             </div>
         </a>

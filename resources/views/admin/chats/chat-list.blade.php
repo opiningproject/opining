227ac35fbdd442717b18ee5afd6fa2176049e6a9
Karan Khimani <karan.k@inheritx.com>
2024-03-28 10:40:10 +0530

    feat: change user status

--- resources/views/admin/chats/chat-list.blade.php
+++ resources/views/admin/chats/chat-list.blade.php
@@ -3,7 +3,7 @@
     <div class="ChatDiv-list">
         <input type="hidden" name="sender_id" value="{{ $chat->sender_id }}" id="sender_id_{{$chat->sender_id}}">
         <input type="hidden" name="receiver_id" value="{{ $chat->receiver_id }}" id="receiver_id_{{$chat->sender_id}}">
-        <input type="hidden" name="user_status" value="{{ $chat->sender->is_online }}" id="user_status_{{$chat->sender_id}}">
+        <input type="hidden" name="user_status" value="{{ $chat->receiver->is_online }}" id="user_status_{{$chat->receiver_id}}">
         <div
             class="ChatDiv-item d-flex align-items-center justify-content-start gap-3" id="chat_item_{{ $chat->receiver_id }}" data-id="{{ $chat->receiver_id }}">
             <img src="images/user-profile.png" alt="Profile-Img" class="img-fluid"

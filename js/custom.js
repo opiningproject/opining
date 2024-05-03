2f34b1a051fa4186debcfebd3343c20f66a10bae
darshan.b1 <darshan.b@inheritx.com>
2024-05-03 17:15:40 +0530

    - new folder structure

--- js/custom.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $("#mobilesearchToggle").click(function(){
-      $(".searcheaderBox").toggleClass("toggled");
-    });
-
-    $("#closeSearch").click(function(){
-        $(".searcheaderBox").removeClass("toggled");
-      });
-  });

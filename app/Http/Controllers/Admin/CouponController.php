55717b10444c1133147bae42395aef55f6ccaa74
Rekha Sharma <rekhas.inheritx@gmail.com>
2024-05-17 19:44:49 +0530

    - client feedback - design - 17-05-2024

--- app/Http/Controllers/Admin/CouponController.php
+++ app/Http/Controllers/Admin/CouponController.php
@@ -20,7 +20,7 @@ class CouponController extends Controller
      */
     public function index()
     {
-        $coupons = Coupon::orderBy('id', 'desc')->get();
+        $coupons = Coupon::orderBy('id', 'desc')->whereNotNull('points')->get();
 
         return view('admin.coupons.index', ['coupons' => $coupons]);
     }

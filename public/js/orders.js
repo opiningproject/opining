59df5dd4a435db2805c55f187c82faf3ee93a94d
darshan.b1 <darshan.b@inheritx.com>
2024-07-08 16:02:09 +0530

    - Modified design in print. - icon issue resolved. - order popup issue resolved.

--- public/js/orders.js
+++ public/js/orders.js
@@ -33,7 +33,9 @@ function orderDetail(id) {
             $('.foodorder-box-details').html(response);
 
             $(".foodorder-box-list div").removeClass("active");
+            $('.foodorder-box-list .time img').attr('src', baseURL + '/images/clock-yellow.svg');
             $('.order-' + id).addClass('active');
+            $('.foodorder-box-list .active .time img').attr('src', baseURL + '/images/clock-black.svg');
         },
         error: function (response) {
             var errorMessage = JSON.parse(response.responseText).message

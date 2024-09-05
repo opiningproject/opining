cd45dcaa5373921463918dc5fd2026258b72ec20
darshan.b1 <darshan.b@inheritx.com>
2024-09-05 18:26:34 +0530

    - Created new pages for checkout, payment setting, api setting, domain setting pages - Added tabs in setting page.

---
+++ resources/views/admin/settings/checkout-setting.blade.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Enums\OrderStatus;
+use App\Enums\OrderType;
+use App\Enums\PaymentStatus;
+use App\Enums\PaymentType;
+use App\Enums\RefundStatus;
+
+?>
+
+<div class="tab-pane fade" id="checkoutSetting-tab-pane" role="tabpanel" aria-labelledby="checkoutSetting-tab" tabindex="0">
+  <div class="card-body">
+    <div class="paymentHistory-card-body  py-3 pt-0">
+        <p> Checkout </p>
+        <div class="border-custom-1"></div>
+      <div class="paymentHistory-table custom-table mt-5">
+        <h2>Coming Soon</h2>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ resources/views/admin/settings/domain-setting.blade.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Enums\OrderStatus;
+use App\Enums\OrderType;
+use App\Enums\PaymentStatus;
+use App\Enums\PaymentType;
+use App\Enums\RefundStatus;
+
+?>
+
+<div class="tab-pane fade" id="domainSetting-tab-pane" role="tabpanel" aria-labelledby="domainSetting-tab" tabindex="0">
+  <div class="card-body">
+    <div class="paymentHistory-card-body  py-3 pt-0">
+        <p> Domain Setting </p>
+        <div class="border-custom-1"></div>
+      <div class="paymentHistory-table custom-table mt-5">
+        <h2>Coming Soon</h2>
+      </div>
+    </div>
+  </div>
+</div>

9bf456ea10283a51be0918b8648873de7f0adcbb
Nishit Sangani <nishit.s@inheritx.com>
2024-09-20 17:05:12 +0530

    Order page modal basic structure

--- resources/views/admin/orders/order-detail-popup.blade.php
+++ resources/views/admin/orders/order-detail-popup.blade.php
@@ -1,21 +1,289 @@
-<div class="modal fade custom-modal order-detail-popup" id="orderDetailModal" tabindex="-1"
-     aria-labelledby="orderDetailModal"
-     aria-hidden="true">
-    <div class="modal-dialog modal-md modal-dialog-centered">
-        <div class="modal-content border-radius">
+<div class="modal fade custom-modal order-detail-popup" style="display: block !important;opacity: 1"
+    id="orderDetailModal" tabindex="-1" aria-labelledby="orderDetailModal" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
             <div class="modal-header border-0">
-                DELIVERY ORDER    #1022
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body pt-1">
-                <div class="orderTop d-flex align-items-center justify-content-between mb-3 border-bottom pb-2">
-
+                <div class="head-flex">
+                    <h3 class="mb-0">DELIVERY ORDER #1022</h3>
+                    <h3 class="mb-0">WEBSITE ORDER</h3>
+                    <h3 class="mb-0">17-09-2024 &nbsp;&nbsp;&nbsp; 18:35</h3>
                 </div>
-                <div class="row justify-content-between">
-                    <div class="col-md-6 mb-3">
-                        <div class="font-14">
+            </div>
+
+            <div class="modal-body pt-1">
+                <div class="border-0 d-flex align-items-center justify-content-between mb-0">
+
+                    <div class="order-status">
+
+                        <div class="order-col">
+                            <label class="status-option">
+                                <input id="test1" type="radio" name="orderStatus" />
+                                <span for="test1"></span>
+                                <label>New Order</label>
+                            </label>
+                        </div>
+
+                        <div class="order-col">
+                            <label class="status-option active">
+                                <input id="test2" type="radio" name="orderStatus" checked />
+                                <span for="test2"></span>
+                                <label>In kitchen</label>
+                            </label>
+                        </div>
+
+                        <div class="order-col">
+                            <label class="status-option">
+                                <input id="test3" type="radio" name="orderStatus" />
+                                <span for="test3"></span>
+                                <label>Out for Delivery</label>
+                            </label>
+                        </div>
+
+                        <div class="order-col">
+                            <label class="status-option">
+                                <input id="test4" type="radio" name="orderStatus" />
+                                <span for="test4"></span>
+                                <label>Delivered</label>
+                            </label>
                         </div>
                     </div>
+
+                </div>
+                <div class="clearfix">
+                    <ul class="nav nav-tabs justify-content-between" id="myTab" role="tablist">
+                        <!-- First Tab -->
+                        <li class="nav-item" role="presentation" style="width: 33%;">
+                            <button class="nav-link active w-100" id="tab-1" data-bs-toggle="tab"
+                                data-bs-target="#content-1" type="button" role="tab" aria-controls="content-1"
+                                aria-selected="true">Customer data</button>
+                        </li>
+                        <!-- Second Tab -->
+                        <li class="nav-item" role="presentation" style="width: 33%;">
+                            <button class="nav-link w-100" id="tab-2" data-bs-toggle="tab" data-bs-target="#content-2"
+                                type="button" role="tab" aria-controls="content-2" aria-selected="false">Order (4
+                                items)</button>
+                        </li>
+                        <!-- Third Tab -->
+                        <li class="nav-item" role="presentation" style="width: 33%;">
+                            <button class="nav-link w-100" id="tab-3" data-bs-toggle="tab" data-bs-target="#content-3"
+                                type="button" role="tab" aria-controls="content-3"
+                                aria-selected="false">Delivered</button>
+                        </li>
+                    </ul>
+
+                    <!-- Tab Content -->
+                    <div class="tab-content px-0">
+                        <!-- Content for Tab 1 -->
+                        <div class="tab-pane fade show active" id="content-1" role="tabpanel" aria-labelledby="tab-1">
+                            <h4>Tab 1 Content</h4>
+                            <p>This is the content for Tab 1.</p>
+                        </div>
+                        <!-- Content for Tab 2 -->
+                        <div class="tab-pane fade" id="content-2" role="tabpanel" aria-labelledby="tab-2">
+                            <div class="row items-in-row">
+
+                                <div class="col-lg-6 mb-3">
+                                    <div class="d-flex ord_listing mb-3">
+                                        <div class="num-items d-flex gap-1">
+                                            <h4>1X</h4>
+
+                                            <div class="item-option">
+                                                <h5>Pollo</h5>
+                                                <h6>Options</h6>
+                                                <ul>
+                                                    <li>+ Sprite (€,00)</li>
+                                                    <li>+ Chilli Sauce (€0,00)</li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <div class="price ms-auto">€14,95</div>
+                                    </div>
+
+                                    <div class="d-flex ord_listing mb-3">
+                                        <div class="num-items d-flex gap-1">
+                                            <h4>1X</h4>
+
+                                            <div class="item-option">
+                                                <h5>Pollo</h5>
+                                                <h6>Options</h6>
+                                                <ul>
+                                                    <li>+ Sprite (€,00)</li>
+                                                    <li>+ Chilli Sauce (€0,00)</li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <div class="price ms-auto">€14,95</div>
+                                    </div>
+
+                                    <div class="d-flex ord_listing mb-3">
+                                        <div class="num-items d-flex gap-1">
+                                            <h4>1X</h4>
+
+                                            <div class="item-option">
+                                                <h5>Pollo</h5>
+                                                <h6>Options</h6>
+                                                <ul>
+                                                    <li>+ Sprite (€,00)</li>
+                                                    <li>+ Chilli Sauce (€0,00)</li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <div class="price ms-auto">€14,95</div>
+                                    </div>
+
+                                    <div class="d-flex ord_listing mb-3">
+                                        <div class="num-items d-flex gap-1">
+                                            <h4>1X</h4>
+
+                                            <div class="item-option">
+                                                <h5>Pollo</h5>
+                                                <h6>Options</h6>
+                                                <ul>
+                                                    <li>+ Sprite (€,00)</li>
+                                                    <li>+ Chilli Sauce (€0,00)</li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <div class="price ms-auto">€14,95</div>
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-6 mb-3">
+                                    <div class="d-flex ord_listing mb-3">
+                                        <div class="num-items d-flex gap-1">
+                                            <h4>1X</h4>
+
+                                            <div class="item-option">
+                                                <h5>Pollo</h5>
+                                                <h6>Options</h6>
+                                                <ul>
+                                                    <li>+ Sprite (€,00)</li>
+                                                    <li>+ Chilli Sauce (€0,00)</li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <div class="price ms-auto">€14,95</div>
+                                    </div>
+
+                                    <div class="d-flex ord_listing mb-3">
+                                        <div class="num-items d-flex gap-1">
+                                            <h4>1X</h4>
+
+                                            <div class="item-option">
+                                                <h5>Pollo</h5>
+                                                <h6>Options</h6>
+                                                <ul>
+                                                    <li>+ Sprite (€,00)</li>
+                                                    <li>+ Chilli Sauce (€0,00)</li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <div class="price ms-auto">€14,95</div>
+                                    </div>
+
+
+                                </div>
+
+                            </div>
+                        </div>
+                        <!-- Content for Tab 3 -->
+                        <div class="tab-pane fade" id="content-3" role="tabpanel" aria-labelledby="tab-3">
+
+                            <div class="order-status order-delivered">
+
+                                <div class="order-col">
+                                    <label class="status-option">
+                                        <input id="test5" type="radio" name="orderStatus" />
+                                        <span for="test5"></span>
+                                        <label>Serdar</label>
+                                    </label>
+                                </div>
+
+                                <div class="order-col">
+                                    <label class="status-option active">
+                                        <input id="test6" type="radio" name="orderStatus" checked />
+                                        <span for="test6"></span>
+                                        <label>Jamal</label>
+                                    </label>
+                                </div>
+
+                                <div class="order-col">
+                                    <label class="status-option">
+                                        <input id="test7" type="radio" name="orderStatus" />
+                                        <span for="test7"></span>
+                                        <label>Krish</label>
+                                    </label>
+                                </div>
+
+                                <div class="order-col">
+                                    <label class="status-option">
+                                        <input id="test8" type="radio" name="orderStatus" />
+                                        <span for="test8"></span>
+                                        <label>Daman</label>
+                                    </label>
+                                </div>
+
+                                <div class="order-col">
+                                    <label class="status-option">
+                                        <input id="test8" type="radio" name="orderStatus" />
+                                        <span for="test8"></span>
+                                        <label>Jack</label>
+                                    </label>
+                                </div>
+
+                                <div class="order-col">
+                                    <label class="status-option">
+                                        <input id="test8" type="radio" name="orderStatus" />
+                                        <span for="test8"></span>
+                                        <label>Peter</label>
+                                    </label>
+                                </div>
+
+                                <div class="order-col">
+                                    <label class="status-option">
+                                        <input id="test8" type="radio" name="orderStatus" />
+                                        <span for="test8"></span>
+                                        <label>Muhammad</label>
+                                    </label>
+                                </div>
+
+                                <div class="order-col">
+                                    <label class="status-option">
+                                        <input id="test8" type="radio" name="orderStatus" />
+                                        <span for="test8"></span>
+                                        <label>Youssef</label>
+                                    </label>
+                                </div>
+
+                                <div class="order-col">
+                                    <label class="status-option">
+                                        <input id="test8" type="radio" name="orderStatus" />
+                                        <span for="test8"></span>
+                                        <label>Sam</label>
+                                    </label>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="modal-footer d-flex align-items-center justify-content-between">
+                <div class="clearfix">
+                    <button class="btn btn-outline-danger text-danger">Cancel</button>
+                    <button
+                        class="btn btn-outline-secondary ms-2 text-secondary d-inline-flex align-items-center gap-3"><img
+                            src="images/print.png" alt="" />Print Label</button>
+                </div>
+
+                <div class="clearfix d-flex align-items-center">
+                    <h3 class="note d-flex align-items-center">SEE NOTE <img class="ms-1" src="images/note.png"
+                            alt="" /></h3>
+                    <h3 class="note d-flex align-items-center ms-3">UNPAID ORDER <img class="ms-1" src="images/note.png"
+                            alt="" /></h3>
+                    <button type="button" class="close-btn ms-3" data-bs-dismiss="modal"
+                        aria-label="Close">Close</button>
                 </div>
             </div>
         </div>

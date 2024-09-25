85a50d663dbc19411da78dc95decc9a0bcbb61b7
darshan.b1 <darshan.b@inheritx.com>
2024-09-25 18:21:43 +0530

    - Plans and bills tab added in settings.

---
+++ resources/views/admin/settings/plans-bills.blade.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Enums\OrderStatus;
+use App\Enums\OrderType;
+use App\Enums\PaymentStatus;
+use App\Enums\PaymentType;
+use App\Enums\RefundStatus;
+
+?>
+
+<div class="tab-pane fade" id="plansBills-tab-pane" role="tabpanel" aria-labelledby="plansBills-tab" tabindex="0">
+  <div class="card-body">
+    <div class="paymentHistory-card-body  py-3 pt-0">
+        <p> {{ trans('rest.settings.plans_bill.title') }} </p>
+        <div class="border-custom-1"></div>
+      <div class="paymentHistory-table custom-table mt-5">
+        <h2>Coming Soon</h2>
+      </div>
+    </div>
+  </div>
+</div>

57e3d166c6a97c9b2ba195988502971e3cdc59be
Nishit Sangani <nishit.s@inheritx.com>
2024-09-26 15:16:42 +0530

    Plans and bill tab static HTML and Update UI of setting page

--- resources/views/admin/settings/api-setting.blade.php
+++ resources/views/admin/settings/api-setting.blade.php
@@ -9,13 +9,12 @@ use App\Enums\RefundStatus;
 ?>
 
 <div class="tab-pane fade" id="apiSetting-tab-pane" role="tabpanel" aria-labelledby="apiSetting-tab" tabindex="0">
-  <div class="card-body">
-    <div class="paymentHistory-card-body  py-3 pt-0">
-        <p> API Setting </p>
-        <div class="border-custom-1"></div>
-      <div class="paymentHistory-table custom-table mt-5">
-        <h2>Coming Soon</h2>
-      </div>
+
+    <h2 class="content-title">API Setting</h2>
+
+    <div class="card-body bg-white">
+        <div class="paymentHistory-card-body pt-0">
+            <h2>Coming Soon</h2>
+        </div>
     </div>
-  </div>
 </div>
